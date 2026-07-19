09d6986dbf7a6b986359184adaaa73d422f1e1d0
Muhammad Noman <nomi.qasim7788@gmail.com>
2026-07-19 13:39:51 +0500

    update backend URL in api.js

--- inventory/src/api.js
+++ inventory/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://inventory-project-gold.vercel.app',
+  baseURL: 'https://inventory-project-gold.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
